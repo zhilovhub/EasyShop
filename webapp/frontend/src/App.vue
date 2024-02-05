c051f350d565c5158fc43e1d49fb6195472cd1b5
karfogent <karfogent3@gmail.com>
2024-02-05 13:50:39 +0300

    Main structure webApp

---
+++ webapp/frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <RouterView />
+  <RouterLink to="/shopping-cart" v-if="itemsAddToCartArray.length>0"><custom-button /></RouterLink>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import CustomButton from '@/components/customButton.vue'
+
+export default defineComponent({
+  components: { CustomButton },
+  computed: {
+    itemsAddToCartArray() {
+      return this.$store.state.itemsAddToCartArray;
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+</style>
