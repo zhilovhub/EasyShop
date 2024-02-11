ae98d2b0aa65c17d4b0989ed6d918099eaecf2a3
root <root@2531804-wd24137.twc1.net>
2024-02-11 23:21:43 +0300

    from server

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 echo "Starting Project..."
 echo "Starting API service... [Step 1/3]"
-systemctl restart api.service
+systemctl enable --now api.service &
+sleep 3
+systemctl status api.service | grep Loaded
 systemctl status api.service | grep Active
 echo "Starting Telegram bot service... [Step 2/3]"
-systemctl restart bot.service
+systemctl enable --now bot.service &
+sleep 3
+systemctl status bot.service | grep Loaded
 systemctl status bot.service | grep Active
 echo "Starting WebApp service... [Step 3/3]"
-systemctl restart webapp.service
+systemctl enable --now webapp.service &
+sleep 3
+systemctl status webapp.service | grep Loaded
 systemctl status webapp.service | grep Active
