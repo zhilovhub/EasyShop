45e4c599a91322a7d88ae44b79be52e06ea02c13
root <root@2531804-wd24137.twc1.net>
2024-02-11 23:30:10 +0300

    change 127.0.0.1 to 0.0.0.0

--- start.sh
+++ start.sh
@@ -1,16 +1,19 @@
 echo "Starting Project..."
 echo "Starting API service... [Step 1/3]"
-systemctl enable --now api.service &
+systemctl enable api.service
+systemctl restart api.service &
 sleep 3
 systemctl status api.service | grep Loaded
 systemctl status api.service | grep Active
 echo "Starting Telegram bot service... [Step 2/3]"
-systemctl enable --now bot.service &
+systemctl enable bot.service
+systemctl restart bot.service &
 sleep 3
 systemctl status bot.service | grep Loaded
 systemctl status bot.service | grep Active
 echo "Starting WebApp service... [Step 3/3]"
-systemctl enable --now webapp.service &
+systemctl enable webapp.service 
+systemctl restart webapp.service &
 sleep 3
 systemctl status webapp.service | grep Loaded
 systemctl status webapp.service | grep Active
