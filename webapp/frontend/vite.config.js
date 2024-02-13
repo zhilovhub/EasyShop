dc3ab1d145d12b7e17e52cdae08b1a9a08d5aa80
root <root@2531804-wd24137.twc1.net>
2024-02-13 19:10:56 +0300

    add https setup

--- webapp/frontend/vite.config.js
+++ webapp/frontend/vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+  https: {
+    key: fs.readFileSync('/root/.certs/key.pem'),
+    cert: fs.readFileSync('/root/.certs/cert.pem')
+  }}
 })
