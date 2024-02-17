973a8e7e604ce7009790a50f215ab8d16f821087
Ilya <zhilovgithub@gmail.com>
2024-02-17 21:17:56 +0300

    return certs

--- webapp/frontend/vite.config.js
+++ webapp/frontend/vite.config.js
@@ -15,5 +15,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+  https: {
+    key: fs.readFileSync('/root/.certs/key.pem'),
+    cert: fs.readFileSync('/root/.certs/cert.pem')
+  }}
 })
