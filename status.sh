cc81d76a5432063a563aa191e8bb4785e573c1f8
Vovan533 <vovan533v@gmail.com>
2024-02-18 17:38:15 +0300

    little services fixes

--- status.sh
+++ status.sh
@@ -2,10 +2,10 @@ echo "Getting services status..."
 echo "API service [1/3]"
 systemctl status api.service | grep Loaded
 systemctl status api.service | grep Active
-echo "Starting Telegram bot service [2/3]"
+echo "Telegram bot service [2/3]"
 systemctl status bot.service | grep Loaded
 systemctl status bot.service | grep Active
-echo "Starting WebApp service [3/3]"
+echo "WebApp service [3/3]"
 systemctl status webapp.service | grep Loaded
 systemctl status webapp.service | grep Active
 
