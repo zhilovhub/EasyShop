2f585c7c58785292efcc1b133857e274d2171025
Vovan533 <vovan533v@gmail.com>
2024-02-25 22:21:28 +0300

    add multibot service and change scripts

--- release_scripts/start.sh
+++ release_scripts/start.sh
@@ -1,13 +1,19 @@
 echo "Starting Project..."
-echo "Starting API service... [Step 1/2]"
+echo "Starting API service... [Step 1/3]"
 systemctl enable release_api.service
 systemctl restart release_api.service &
 sleep 3
 systemctl status release_api.service | grep Loaded
 systemctl status release_api.service | grep Active
-echo "Starting Telegram bot service... [Step 2/2]"
+echo "Starting Telegram bot service... [Step 2/3]"
 systemctl enable release_bot.service
 systemctl restart release_bot.service &
 sleep 3
 systemctl status release_bot.service | grep Loaded
 systemctl status release_bot.service | grep Active
+echo "Starting MultiBot service... [Step 3/3]"
+systemctl enable release_multibot.service
+systemctl restart release_multibot.service &
+sleep 3
+systemctl status release_multibot.service | grep Loaded
+systemctl status release_multibot.service | grep Active
--- release_scripts/status.sh
+++ release_scripts/status.sh
@@ -1,7 +1,10 @@
 echo "Getting services status..."
-echo "API service [1/2]"
+echo "API service [1/3]"
 systemctl status release_api.service | grep Loaded
 systemctl status release_api.service | grep Active
-echo "Telegram bot service [2/2]"
+echo "Telegram bot service [2/3]"
 systemctl status release_bot.service | grep Loaded
 systemctl status release_bot.service | grep Active
+echo "MultiBot service [3/3]"
+systemctl status release_multibot.service | grep Loaded
+systemctl status release_multibot.service | grep Active
