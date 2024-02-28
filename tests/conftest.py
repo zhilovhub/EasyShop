71eaeb27333e06174410483188cdf64866bdadfe
Ilya Zhilov <zhilovgithub@gmail.com>
2024-02-28 18:31:38 +0300

    UserDao tests

---
+++ tests/conftest.py
@@ -0,0 +1,43 @@
+import asyncio
+import os
+import sys
+
+from dotenv import load_dotenv
+
+import pytest
+
+from database.models.models import Database
+from database.models.user_model import UserDao
+
+from database.models.models import Base
+
+load_dotenv()
+
+
+@pytest.fixture
+async def database() -> Database:
+    database = Database(sqlalchemy_url=os.environ["DB_FOR_TESTS"])
+    await database.connect()
+    yield database
+    async with database.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture
+def user_db(database: Database) -> UserDao:
+    return database.get_user_dao()
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    """
+    Creates an instance of the default event loop for the test session.
+    """
+    if sys.platform.startswith("win") and sys.version_info[:2] >= (3, 8):
+        # Avoid "RuntimeError: Event loop is closed" on Windows when tearing down tests
+        # https://github.com/encode/httpx/issues/914
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
---
+++ tests/test_user_db.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+
+import pytest
+
+from bot.exceptions import InvalidParameterFormat, UserNotFound, InstanceAlreadyExists
+from database.models.user_model import UserDao, UserSchema
+
+
+user_schema_1 = UserSchema(
+        user_id=1,
+        status="new",
+        registered_at=datetime.utcnow()
+)
+user_schema_2 = UserSchema(
+        user_id=2,
+        status="new",
+        registered_at=datetime.utcnow()
+)
+
+
+@pytest.fixture
+async def before_add_user(user_db: UserDao) -> None:
+    await user_db.add_user(user_schema_1)
+
+
+@pytest.fixture
+async def before_add_two_users(user_db: UserDao) -> None:
+    await user_db.add_user(user_schema_1)
+    await user_db.add_user(user_schema_2)
+
+
+class TestUserDb:
+    async def test_get_user(self, user_db: UserDao, before_add_user) -> None:
+        """UserDao.get_user"""
+        with pytest.raises(InvalidParameterFormat):
+            await user_db.get_user(str(user_schema_1.id))
+
+        with pytest.raises(UserNotFound):
+            await user_db.get_user(user_schema_1.id + 1)
+
+        user = await user_db.get_user(user_schema_1.id)
+        assert user == user_schema_1
+        
+    async def test_get_users(self, user_db: UserDao, before_add_two_users) -> None:
+        """UserDao.get_users"""
+        users = await user_db.get_users()
+        assert users[0] == user_schema_1
+        assert users[1] == user_schema_2
+
+    async def test_add_user(self, user_db: UserDao, before_add_user) -> None:
+        """UserDao.add_user"""
+        with pytest.raises(InvalidParameterFormat):
+            await user_db.add_user(1)
+
+        with pytest.raises(InstanceAlreadyExists):
+            await user_db.add_user(user_schema_1)
+
+        await user_db.add_user(user_schema_2)
+        user = await user_db.get_user(user_schema_2.id)
+        assert user == user_schema_2
+
+    async def test_update_user(self, user_db: UserDao, before_add_user) -> None:
+        """UserDao.update_user"""
+        with pytest.raises(InvalidParameterFormat):
+            await user_db.update_user(1)
+
+        user_schema_1.status = "new_status"
+        await user_db.update_user(user_schema_1)
+        user = await user_db.get_user(user_schema_1.id)
+        assert user.status == "new_status"
+
+    async def test_del_user(self, user_db: UserDao, before_add_user) -> None:
+        """UserDao.del_user"""
+        with pytest.raises(InvalidParameterFormat):
+            await user_db.del_user(user_schema_1)
+
+        await user_db.get_user(user_schema_1.id)
+        await user_db.del_user(user_schema_1.id)
+
+        with pytest.raises(UserNotFound):
+            await user_db.get_user(user_schema_1.id)
