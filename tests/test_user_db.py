b14a153c2ddf4a293d53e355af690d68f610bbb2
Vovan533 <vovan533v@gmail.com>
2024-03-09 23:54:06 +0300

    change user schema in tests

--- tests/test_user_db.py
+++ tests/test_user_db.py
@@ -1,20 +1,23 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
 
 from bot.exceptions import InvalidParameterFormat, UserNotFound, InstanceAlreadyExists
-from database.models.user_model import UserDao, UserSchema
+from database.models.user_model import UserDao, UserSchema, NotInUserStatusesList
 
+from pydantic import ValidationError
 
 user_schema_1 = UserSchema(
-        user_id=1,
-        status="new",
-        registered_at=datetime.utcnow()
+    user_id=1,
+    status="new",
+    subscribed_until=None,
+    registered_at=datetime.utcnow()
 )
 user_schema_2 = UserSchema(
-        user_id=2,
-        status="new",
-        registered_at=datetime.utcnow()
+    user_id=2,
+    status="new",
+    subscribed_until=datetime.now() + timedelta(days=7),
+    registered_at=datetime.utcnow()
 )
 
 
@@ -40,7 +43,7 @@ class TestUserDb:
 
         user = await user_db.get_user(user_schema_1.id)
         assert user == user_schema_1
-        
+
     async def test_get_users(self, user_db: UserDao, before_add_two_users) -> None:
         """UserDao.get_users"""
         users = await user_db.get_users()
@@ -64,10 +67,15 @@ class TestUserDb:
         with pytest.raises(InvalidParameterFormat):
             await user_db.update_user(1)
 
-        user_schema_1.status = "new_status"
+        with pytest.raises(ValidationError):
+            user_schema_1.status = "random_string"
+            await user_db.update_user(user_schema_1)
+            user = await user_db.get_user(user_schema_1.id)
+
+        user_schema_1.status = "trial"
         await user_db.update_user(user_schema_1)
         user = await user_db.get_user(user_schema_1.id)
-        assert user.status == "new_status"
+        assert user.status == "trial"
 
     async def test_del_user(self, user_db: UserDao, before_add_user) -> None:
         """UserDao.del_user"""
