efafd745b6cb9f77e936c6394f2afcd3015d4978
Ilya Zhilov <zhilovgithub@gmail.com>
2024-03-11 11:42:02 +0300

    Scheduler now singleton

--- bot/utils/scheduler.py
+++ bot/utils/scheduler.py
@@ -15,6 +15,18 @@ logging.config.dictConfig(config.LOGGING_SETUP)
 logger = logging.getLogger('logger')
 
 
+def singleton(class_):
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+
+    return get_instance
+
+
+@singleton
 class Scheduler:
 
     def __init__(self, scheduler: AsyncIOScheduler, jobstore_alias: str, timezone: str):
