c21d07afd9f5e9e062cc28ccbd63696589bc1f03
Ilya <zhilovgithub@gmail.com>
2024-03-16 08:59:54 +0300

    added try except to send_event

--- bot/utils/admin_group.py
+++ bot/utils/admin_group.py
@@ -1,12 +1,13 @@
-import os
-
 from aiogram.types import Message
 
-from bot.main import bot, config
+from bot.main import bot, config, logger
 
 
 async def send_event(message: Message) -> None:
-    await bot.send_message(
-        chat_id=config.ADMIN_GROUP_ID,
-        text=message.text
-    )
+    try:
+        await bot.send_message(
+            chat_id=config.ADMIN_GROUP_ID,
+            text=message.text
+        )
+    except Exception as e:
+        logger.info(e)
