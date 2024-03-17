f5813ed4a5c20d64ecd11dae0aed93ddb4620868
Vovan533 <vovan533v@gmail.com>
2024-03-17 22:53:49 +0300

    change bot status on local api call

--- bot/handlers/handlers.py
+++ bot/handlers/handlers.py
@@ -104,6 +104,9 @@ async def start_custom_bot(bot_id: int):
     async with aiohttp.ClientSession() as session:
         async with session.get(
                 f"http://{config.LOCAL_API_SERVER_HOST}:{config.LOCAL_API_SERVER_PORT}/start_bot/{bot_id}") as response:
+            user_bot = await bot_db.get_bot(bot_id)
+            user_bot.status = "online"
+            await bot_db.update_bot(user_bot)
             if response.status != 200:
                 raise LocalAPIException(f"API returned {response.status} status code "
                                         f"with text {await response.text()}")
@@ -113,6 +116,9 @@ async def stop_custom_bot(bot_id: int):
     async with aiohttp.ClientSession() as session:
         async with session.get(
                 f"http://{config.LOCAL_API_SERVER_HOST}:{config.LOCAL_API_SERVER_PORT}/stop_bot/{bot_id}") as response:
+            user_bot = await bot_db.get_bot(bot_id)
+            user_bot.status = "offline"
+            await bot_db.update_bot(user_bot)
             if response.status != 200:
                 raise LocalAPIException(f"API returned {response.status} status code "
                                         f"with text {await response.text()}")
