37fa8677c3d3df28199b95c24428734d745beadf
Ilya Zhilov <zhilovgithub@gmail.com>
2024-03-22 11:14:05 +0300

    updated custom_bot_launching.py

--- bot/handlers/handlers.py
+++ bot/handlers/handlers.py
@@ -101,30 +101,6 @@ async def debug_clear(message: Message, state: FSMContext) -> None:
     await CheckSubscriptionMiddleware().__call__(start_command_handler, message, state)
 
 
-async def start_custom_bot(bot_id: int):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-                f"http://{config.LOCAL_API_SERVER_HOST}:{config.LOCAL_API_SERVER_PORT}/start_bot/{bot_id}") as response:
-            user_bot = await bot_db.get_bot(bot_id)
-            user_bot.status = "online"
-            await bot_db.update_bot(user_bot)
-            if response.status != 200:
-                raise LocalAPIException(f"API returned {response.status} status code "
-                                        f"with text {await response.text()}")
-
-
-async def stop_custom_bot(bot_id: int):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-                f"http://{config.LOCAL_API_SERVER_HOST}:{config.LOCAL_API_SERVER_PORT}/stop_bot/{bot_id}") as response:
-            user_bot = await bot_db.get_bot(bot_id)
-            user_bot.status = "offline"
-            await bot_db.update_bot(user_bot)
-            if response.status != 200:
-                raise LocalAPIException(f"API returned {response.status} status code "
-                                        f"with text {await response.text()}")
-
-
 async def send_subscription_expire_notify(user: UserSchema):
     actual_user = await user_db.get_user(user.id)
 
