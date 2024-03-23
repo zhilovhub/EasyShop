f99974c2d85b0a52ca462bb754631f5ad71e3e36
Ilya Zhilov <zhilovgithub@gmail.com>
2024-03-23 13:31:33 +0300

    changed logic of routers

--- bot/handlers/routers.py
+++ bot/handlers/routers.py
@@ -5,16 +5,13 @@ from bot.middlewaries.subscription_middleware import CheckSubscriptionMiddleware
 
 custom_bot_editing_router = Router(name="custom_bot_editing")  # only for SUBSCRIBERS
 custom_bot_editing_router.message.filter(ChatTypeFilter(chat_type='private'))
+custom_bot_editing_router.message.middleware(CheckSubscriptionMiddleware())
+custom_bot_editing_router.callback_query.middleware(CheckSubscriptionMiddleware())
 
 admin_bot_menu_router = Router(name="admin_bot_menu")  # only for SUBSCRIBERS
 admin_bot_menu_router.message.filter(ChatTypeFilter(chat_type='private'))
-
-for_subscribers_router = Router(name="for_subscribers_router")  # only for SUBSCRIBERS
-for_subscribers_router.message.filter(ChatTypeFilter(chat_type='private'))
-for_subscribers_router.message.middleware(CheckSubscriptionMiddleware())
-for_subscribers_router.callback_query.middleware(CheckSubscriptionMiddleware())
-for_subscribers_router.include_router(admin_bot_menu_router)
-for_subscribers_router.include_router(custom_bot_editing_router)
+admin_bot_menu_router.message.middleware(CheckSubscriptionMiddleware())
+admin_bot_menu_router.callback_query.middleware(CheckSubscriptionMiddleware())
 
 subscribe_router = Router(name="subscribe_router")
 subscribe_router.message.filter(ChatTypeFilter(chat_type='private'))
