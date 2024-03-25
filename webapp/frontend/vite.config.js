df71cbbc7cdcd8b8fc5aeb73785fe3e6fd08408e
Ilya Zhilov <zhilovgithub@gmail.com>
2024-03-25 11:52:55 +0300

    added autodeploy to ci.yml

--- webapp/frontend/vite.config.js
+++ webapp/frontend/vite.config.js
@@ -1,9 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import fs from 'fs';
+
+const env = loadEnv(
+    'all',
+    process.cwd()
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,9 +21,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+
   server: {
     https: {
-      key: fs.readFileSync('/root/.certs/key.pem'),
-      cert: fs.readFileSync('/root/.certs/cert.pem')
+      key: fs.readFileSync(env.VITE_SSL_KEY_PATH),
+      cert: fs.readFileSync(env.VITE_SSL_CERT_PATH)
     }}
 })
