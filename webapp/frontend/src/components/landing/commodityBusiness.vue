b61adf4ac49b3649254c5b56e6b206910330c6a0
karfogent <karfogent3@gmail.com>
2024-04-17 21:51:35 +0300

    test#6

--- webapp/frontend/src/components/landing/commodityBusiness.vue
+++ webapp/frontend/src/components/landing/commodityBusiness.vue
@@ -3,9 +3,8 @@
 import { Swiper, SwiperSlide } from 'swiper/vue';
 
 
-// modules styles
-import 'swiper/components/navigation/navigation.min.css'
-import 'swiper/components/pagination/pagination.min.css'
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
 
 // import required modules
 import { Navigation } from 'swiper/modules';
