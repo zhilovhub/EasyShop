c4df1ff0a8ca0f7cec27bcae1c3b6cfda7b719da
karfogent <karfogent3@gmail.com>
2024-04-18 13:38:15 +0300

    test#14

--- webapp/frontend/vite.config.js
+++ webapp/frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import fs from 'fs';
 
 const env = loadEnv(
-    'all',
-    process.cwd()
+  'all',
+  process.cwd()
 );
 
 // https://vitejs.dev/config/
@@ -21,16 +20,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-
   server: {
     https: {
       key: fs.readFileSync(env.VITE_SSL_KEY_PATH),
       cert: fs.readFileSync(env.VITE_SSL_CERT_PATH)
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['swiper', 'swiper/vue', 'swiper/swiper-bundle.min.css', 'swiper/swiper.min.css', 'swiper/modules', 'swiper/css', 'swiper/css/navigation']
     }
   }
 })
