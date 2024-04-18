7bd77c9caeac3ea7afdd0fd6b384077aa63de8b0
karfogent <karfogent3@gmail.com>
2024-04-18 14:47:41 +0300

    test#20

--- webapp/frontend/vite.config.js
+++ webapp/frontend/vite.config.js
@@ -11,6 +11,31 @@ const env = loadEnv(
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  modules: [
+    'virtual',
+    'keyboard',
+    'mousewheel',
+    'navigation',
+    'pagination',
+    'scrollbar',
+    'parallax',
+    'zoom',
+    'controller',
+    'a11y',
+    'history',
+    'hash-navigation',
+    'autoplay',
+    'thumbs',
+    'free-mode',
+    'grid',
+    'manipulation',
+    'effect-fade',
+    'effect-cube',
+    'effect-flip',
+    'effect-coverflow',
+    'effect-creative',
+    'effect-cards',
+  ],
   plugins: [
     vue(),
     vueJsx(),
