632b9f111901fccd632e2e519e72e34b7cd7c74a
Konstantin <94746825+karfogent@users.noreply.github.com>
2024-04-24 18:08:40 +0300

    adaptive bugs are fixed (#144)

--- webapp/frontend/src/components/landing/landingPage.vue
+++ webapp/frontend/src/components/landing/landingPage.vue
@@ -1,6 +1,6 @@
 <script>
 import CommodityBusiness from './commodityBusiness.vue';
-import ServicesSector from './servicesSector.vue';
+// import ServicesSector from './servicesSector.vue';
 export default {
   data() {
     return {
@@ -9,7 +9,7 @@ export default {
   },
   components: {
     CommodityBusiness,
-    ServicesSector
+    // ServicesSector
   },
   mounted() {
     document.getElementById('scrollButton').addEventListener('click', function() {
@@ -41,41 +41,8 @@ export default {
 </script>
 
 <template>
-  <svg style="position: absolute" width="100%" height="343" viewBox="0 0 1920 343" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="39.6416" y="-201.808" width="394.523" height="390.959" rx="43.5" transform="rotate(45 39.6416 -201.808)" stroke="url(#paint0_linear_0_1)" stroke-width="13"/>
-    <rect x="302.821" y="-69.636" width="223.296" height="221.264" rx="45.5" transform="rotate(45 302.821 -69.636)" stroke="url(#paint1_linear_0_1)" stroke-width="9"/>
-    <rect x="1817.82" y="-108.808" width="219.296" height="217.264" rx="43.5" transform="rotate(45 1817.82 -108.808)" stroke="url(#paint2_linear_0_1)" stroke-width="13"/>
-    <rect x="1649.41" y="91.5355" width="94.0274" height="93.1613" rx="17.5" transform="rotate(45 1649.41 91.5355)" stroke="url(#paint3_linear_0_1)" stroke-width="5"/>
-    <rect x="497.41" y="123.536" width="94.0274" height="93.1613" rx="17.5" transform="rotate(45 497.41 123.536)" stroke="url(#paint4_linear_0_1)" stroke-width="5"/>
-    <rect x="858.913" y="-64.4645" width="137.546" height="136.299" rx="17.5" transform="rotate(45 858.913 -64.4645)" stroke="url(#paint5_linear_0_1)" stroke-width="5"/>
-    <defs>
-      <linearGradient id="paint0_linear_0_1" x1="243.403" y1="-211" x2="243.403" y2="192.959" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#168ECD"/>
-        <stop offset="1" stop-color="#8705AB"/>
-      </linearGradient>
-      <linearGradient id="paint1_linear_0_1" x1="418.969" y1="-76" x2="418.969" y2="154.264" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#168ECD"/>
-        <stop offset="1" stop-color="#8705AB"/>
-      </linearGradient>
-      <linearGradient id="paint2_linear_0_1" x1="1933.97" y1="-118" x2="1933.97" y2="112.264" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#168ECD"/>
-        <stop offset="1" stop-color="#8705AB"/>
-      </linearGradient>
-      <linearGradient id="paint3_linear_0_1" x1="1698.92" y1="88" x2="1698.92" y2="186.161" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#168ECD"/>
-        <stop offset="1" stop-color="#8705AB"/>
-      </linearGradient>
-      <linearGradient id="paint4_linear_0_1" x1="546.924" y1="120" x2="546.924" y2="218.161" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#168ECD"/>
-        <stop offset="1" stop-color="#8705AB"/>
-      </linearGradient>
-      <linearGradient id="paint5_linear_0_1" x1="930.186" y1="-68" x2="930.186" y2="73.2988" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#168ECD"/>
-        <stop offset="1" stop-color="#8705AB"/>
-      </linearGradient>
-    </defs>
-  </svg>
   <header>
+    <img style="position: absolute; top: 0; left: 0; width: 100%" src="@/assets/landing/squares-background.svg" alt="squares-background">
     <img class="phones" src="@/assets/landing/phones.png" alt="phones">
     <div class="title-div">
       <img src="@/assets/landing/logo.svg" alt="logo">
@@ -105,6 +72,7 @@ export default {
 }
 
 header{
+  position: relative;
   height: 100vh;
   background-color: #0A1526;
   padding: 0 150px;
@@ -112,7 +80,7 @@ header{
     display: flex;
     flex-direction: column;
     align-items: baseline;
-    position: absolute;
+    position: relative;
     color: #DBE0E9;
     top: 25%;
     max-width: 750vw;
@@ -286,10 +254,10 @@ header{
   }
 }
 
-@media screen and (max-width: 1400px) {
+@media screen and (max-width: 1600px) {
   header {
     .title-div {
-      max-width: 40%;
+      max-width: 50%;
       div {
         font-size: 40px;
         span {
