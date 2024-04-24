2e1437208d639d728718c63cc6b695ca7a2b902d
Ilya Zhilov <zhilovgithub@gmail.com>
2024-04-24 18:39:42 +0300

    added add_product

--- api/products/router.py
+++ api/products/router.py
@@ -1,4 +1,3 @@
-from database.models.category_model import CategorySchema, CategoryDao
 from database.models.product_model import ProductSchema, ProductNotFound, ProductWithoutId, ProductDao
 from loader import db_engine, logger
 from fastapi import HTTPException, APIRouter
@@ -10,14 +9,13 @@ router = APIRouter(
     tags=["products"],
     responses={404: {"description": "Product not found"}},
 )
-product_db: ProductDao = db_engine.get_product_db()
-category_db: CategoryDao = db_engine.get_category_dao()
+db: ProductDao = db_engine.get_product_db()
 
 
 @router.get("/get_all_products/{bot_id}")
 async def get_all_products_api(bot_id: int) -> list[ProductSchema]:
     try:
-        products = await product_db.get_all_products(bot_id)
+        products = await db.get_all_products(bot_id)
     except Exception:
         logger.error("Error while execute get_all_products db_method", exc_info=True)
         raise HTTPException(status_code=500, detail="Internal error.")
@@ -27,7 +25,7 @@ async def get_all_products_api(bot_id: int) -> list[ProductSchema]:
 @router.get("/get_product/{bot_id}/{product_id}")
 async def get_product_api(bot_id: int, product_id: int) -> ProductSchema:
     try:
-        product = await product_db.get_product(product_id)
+        product = await db.get_product(product_id)
     except ProductNotFound:
         raise HTTPException(status_code=404, detail="Product not found.")
     except Exception:
@@ -39,18 +37,8 @@ async def get_product_api(bot_id: int, product_id: int) -> ProductSchema:
 @router.post("/add_product")
 async def add_product_api(new_product: ProductWithoutId) -> int:
     try:
-        product_id = await product_db.add_product(new_product)
+        product_id = await db.add_product(new_product)
     except Exception:
         logger.error("Error while execute add_product db_method", exc_info=True)
         raise HTTPException(status_code=500, detail="Internal error.")
     return product_id
-
-
-@router.get("/get_all_categories/{bot_id}")
-async def get_all_categories_api(bot_id: int) -> list[CategorySchema]:
-    try:
-        categories = await category_db.get_all_categories(bot_id)
-    except Exception:
-        logger.error("Error while execute get_all_categories db_method", exc_info=True)
-        raise HTTPException(status_code=500, detail="Internal error.")
-    return categories
