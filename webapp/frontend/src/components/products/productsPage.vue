d08bcf2603845182186daf455ec39277130c3629
karfogent <karfogent3@gmail.com>
2024-05-07 20:58:09 +0300

    changed url

--- webapp/frontend/src/components/products/productsPage.vue
+++ webapp/frontend/src/components/products/productsPage.vue
@@ -118,7 +118,7 @@ import FilterComponent from '/src/components/products/filterComponent.vue'
 let tg = window.Telegram.WebApp;
 
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-  window.location.href = "/products-page/shopping-cart`"
+  window.location.href = "/products-page/shopping-cart/"
 });
 
 export default {
