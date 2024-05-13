b296209dac1c6b690d5154f9602930201822b49e
Vovan533 <vovan533v@gmail.com>
2024-05-13 22:52:01 +0300

    add filters to get all products request in api method

--- api/products/router.py
+++ api/products/router.py
@@ -1,5 +1,6 @@
 from database.models.category_model import CategorySchema, CategoryDao
-from database.models.product_model import ProductSchema, ProductNotFound, ProductWithoutId, ProductDao
+from database.models.product_model import (ProductSchema, ProductNotFound, ProductWithoutId, ProductDao,
+                                           ProductFilter, ProductFilterWithoutBot, FilterNotFound, PRODUCT_FILTERS)
 from loader import db_engine, logger
 from fastapi import HTTPException, APIRouter, File, UploadFile, Body, Depends
 from typing import Annotated, List
@@ -16,56 +17,16 @@ product_db: ProductDao = db_engine.get_product_db()
 category_db: CategoryDao = db_engine.get_category_dao()
 
 
-class FilterNotFound(Exception):
-    """Raised when filter name provided to ProductFilter class is not a valid filter name from filters list"""
-
-    def __init__(self, filter_name: str, message: str="Provided filter name ('{FILTER_NAME}') not found"):
-        self.filter_name = filter_name
-        self.message = message.replace("{FILTER_NAME}", filter_name.lower())
-        super().__init__(self.message)
-
-
-# class CategoryFilterNotFound(Exception):
-#     """Raised when category name provided to ProductFilter class is not exist in categories table"""
-#
-#     def __init__(self, category_name: str, message: str = "Provided category name ('{CAT_NAME}') for category filter not found"):
-#         self.category_name = category_name
-#         self.message = message.replace("{{CAT_NAME}}", category_name.lower())
-#         super().__init__(self.message)
-
-
-PRODUCT_FILTERS = ("rating", "popular", "price")
-
-
-class ProductFilterWithoutBot(BaseModel):
-    filter_name: Annotated[str, Field(validate_default=True)]
-    is_category_filter: bool = False
-    reverse_order: bool = False
-
-
-class ProductFilter(ProductFilterWithoutBot):
-    bot_id: int = Field(frozen=True)
-
-    @model_validator(mode='after')
-    def validate_filter_name(self):
-        if self.is_category_filter:
-            pass
-            # cats = await category_db.get_all_categories(self.bot_id)
-            # for cat in cats:
-            #     if cat.name.lower() == self.filter_name.lower():
-            #         break
-            # else:
-            #     raise CategoryFilterNotFound(self.filter_name)
-        elif self.filter_name.lower() not in PRODUCT_FILTERS:
-            raise FilterNotFound(self.filter_name)
-        return self
-
-
 class GetProductsRequest(BaseModel):
     bot_id: int = Field(frozen=True)
     price_min: int = 0
     price_max: int = 2147483647
     filters: list[ProductFilterWithoutBot] | None
+
+
+@router.get("/get_filters/")
+async def get_filters_api():
+    return PRODUCT_FILTERS
 
 
 @router.post("/get_all_products/")
@@ -78,12 +39,26 @@ async def get_all_products_api(payload: GetProductsRequest = Depends(GetProducts
         else:
             filters: list[ProductFilter] = []
             for product_filter in payload.filters:
-                filters.append(ProductFilter(bot_id=payload.bot_id, filter_name=product_filter.filter_name,
-                                             is_category_filter=product_filter.is_category_filter,
-                                             reverse_order=product_filter.reverse_order))
+                if product_filter.is_category_filter:
+                    cats = await category_db.get_all_categories(payload.bot_id)
+                    cat_id = None
+                    for cat in cats:
+                        if cat.name.lower() == product_filter.filter_name.lower():
+                            cat_id = cat.id
+                            break
+                    filters.append(ProductFilter(bot_id=payload.bot_id, filter_name=product_filter.filter_name,
+                                                 is_category_filter=product_filter.is_category_filter,
+                                                 reverse_order=product_filter.reverse_order,
+                                                 category_id=cat_id))
+                else:
+                    filters.append(ProductFilter(bot_id=payload.bot_id, filter_name=product_filter.filter_name,
+                                                 is_category_filter=product_filter.is_category_filter,
+                                                 reverse_order=product_filter.reverse_order,
+                                                 category_id=None))
             products = await product_db.get_all_products(payload.bot_id,
                                                          price_min=payload.price_min,
-                                                         price_max=payload.price_max)
+                                                         price_max=payload.price_max,
+                                                         filters=filters)
     except FilterNotFound as ex:
         raise HTTPException(status_code=400, detail=ex.message)
     # except CategoryFilterNotFound as ex:
