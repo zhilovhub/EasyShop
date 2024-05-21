a86e06b9b50821d15c725307fc48c7d493c9ae90
Vovan533 <vovan533v@gmail.com>
2024-05-21 12:39:26 +0300

    add npm run build to restart script

--- debug_scripts/start.sh
+++ debug_scripts/start.sh
@@ -1,19 +1,22 @@
 echo "[DEBUG] Starting Project..."
-echo "[DEBUG] Starting API service... [Step 1/3]"
+echo "[DEBUG] Starting API service... [Step 1/4]"
 systemctl --user enable debug_api.service
 systemctl --user restart debug_api.service &
 sleep 3
 systemctl --user status debug_api.service | grep Loaded
 systemctl --user status debug_api.service | grep Active
-echo "[DEBUG] Starting Telegram bot service... [Step 2/3]"
+echo "[DEBUG] Starting Telegram bot service... [Step 2/4]"
 systemctl --user enable debug_bot.service
 systemctl --user restart debug_bot.service &
 sleep 3
 systemctl --user status debug_bot.service | grep Loaded
 systemctl --user status debug_bot.service | grep Active
-echo "[DEBUG] Starting MultiBot service... [Step 3/3]"
+echo "[DEBUG] Starting MultiBot service... [Step 3/4]"
 systemctl --user enable debug_multibot.service
 systemctl --user restart debug_multibot.service &
 sleep 3
 systemctl --user status debug_multibot.service | grep Loaded
 systemctl --user status debug_multibot.service | grep Active
+echo "[DEBUG] Building webapp frontend... [Step 4/4]"
+cd webapp/frontend
+npm run build
