eff8e7fb063c795218a7147f5b5b0164365d39a8
unknown <zhilovgithub@gmail.com>
2024-05-22 11:47:21 +0300

    returned start.sh

--- debug_scripts/start.sh
+++ debug_scripts/start.sh
@@ -1,7 +1,19 @@
 echo "[DEBUG] Starting Project..."
-echo "[Channel] Starting MultiBot service... [Step 1/1]"
-systemctl --user enable channel_multibot.service
-systemctl --user restart channel_multibot.service &
+echo "[DEBUG] Starting API service... [Step 1/3]"
+systemctl --user enable debug_api.service
+systemctl --user restart debug_api.service &
 sleep 3
-systemctl --user status channel_multibot.service | grep Loaded
-systemctl --user status channel_multibot.service | grep Active
+systemctl --user status debug_api.service | grep Loaded
+systemctl --user status debug_api.service | grep Active
+echo "[DEBUG] Starting Telegram bot service... [Step 2/3]"
+systemctl --user enable debug_bot.service
+systemctl --user restart debug_bot.service &
+sleep 3
+systemctl --user status debug_bot.service | grep Loaded
+systemctl --user status debug_bot.service | grep Active
+echo "[DEBUG] Starting MultiBot service... [Step 3/3]"
+systemctl --user enable debug_multibot.service
+systemctl --user restart debug_multibot.service &
+sleep 3
+systemctl --user status debug_multibot.service | grep Loaded
+systemctl --user status debug_multibot.service | grep Active
