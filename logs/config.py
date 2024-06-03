1d0983a04f14fba02e2cfd6452ef9a7c3904d077
Ilya <zhilovgithub@gmail.com>
2024-06-03 14:06:12 +0300

    file_handler + file_error_warning_handler finished

--- logs/config.py
+++ logs/config.py
@@ -13,10 +13,26 @@ class LokiFilter(logging.Filter):
         return True
 
 
+class ErrorWarningFilter(logging.Filter):
+
+    def filter(self, record: LogRecord) -> bool:
+        if record.levelno >= 30:
+            return True
+
+        return False
+
+
+load_dotenv()
+
+DEBUG = bool(int(os.getenv("DEBUG")))
+LOGS_PATH = os.getenv("PROJECT_ROOT") + "logs/"
+
+FORMATTER_NAME = "formatter"
+
 logger_configuration = {
     "version": 1,
     "formatters": {
-        "formatter": {
+        FORMATTER_NAME: {
             "format": "{levelname} {asctime} {filename} {funcName}() {msg}",
             "style": "{"
         }
@@ -24,23 +40,39 @@ logger_configuration = {
     "filters": {
         "loki_filter": {
             "()": LokiFilter
+        },
+        "error_warning_filter": {
+            "()": ErrorWarningFilter
         }
     },
     "handlers": {
         "console_handler": {
             "class": "logging.StreamHandler",
             "level": "DEBUG",
-            "formatter": "formatter",
+            "formatter": FORMATTER_NAME,
+        },
+        "file_handler": {
+            "class": "logging.FileHandler",
+            "level": "DEBUG",
+            "formatter": FORMATTER_NAME,
+            "filename": LOGS_PATH + "all.log"
+        },
+        "file_error_warning_handler": {
+            "class": "logging.FileHandler",
+            "level": "WARNING",
+            "formatter": FORMATTER_NAME,
+            "filename": LOGS_PATH + "err.log",
+            "filters": ["error_warning_filter"]
         }
     },
     "loggers": {
         "debug_logger": {
             "level": "DEBUG",
-            "handlers": ["console_handler"]
+            "handlers": ["console_handler", "file_handler", "file_error_warning_handler"]
         },
         "release_logger": {
             "level": "DEBUG",
-            "handlers": ["console_handler"]
+            "handlers": ["console_handler", "file_handler", "file_error_warning_handler"]
         }
     }
 }
@@ -48,10 +80,12 @@ logger_configuration = {
 
 def main():
     logger.info("hi", extra={"uh": 12})
+    logger.info("hi1")
+    logger.warning("hi2")
+    logger.warning("hi2")
+    logger.info("hi3")
+    logger.error("hi4")
 
-
-load_dotenv()
-DEBUG = bool(int(os.getenv("DEBUG")))
 
 logging.config.dictConfig(logger_configuration)
 logger = logging.getLogger("debug_logger" if DEBUG else "release_logger")
