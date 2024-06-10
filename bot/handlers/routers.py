d32ac267cc01b30dd8ad78b4aeeedb751ae30d1d
unknown <zhilovgithub@gmail.com>
2024-06-10 14:05:06 +0300

    log_middlware

--- bot/handlers/routers.py
+++ bot/handlers/routers.py
@@ -3,35 +3,46 @@ from aiogram import Router
 from bot.filters import ChatTypeFilter
 from bot.middlewaries.subscription_middleware import CheckSubscriptionMiddleware
 from bot.middlewaries.errors_middleware import ErrorMiddleware
+from bot.middlewaries.log_middleware import MainLogMiddleware
 
 custom_bot_editing_router = Router(name="custom_bot_editing")  # only for SUBSCRIBERS
 custom_bot_editing_router.message.filter(ChatTypeFilter(chat_type="private"))
+custom_bot_editing_router.message.outer_middleware(MainLogMiddleware())
 custom_bot_editing_router.message.middleware(CheckSubscriptionMiddleware())
+custom_bot_editing_router.callback_query.outer_middleware(MainLogMiddleware())
 custom_bot_editing_router.callback_query.middleware(CheckSubscriptionMiddleware())
 custom_bot_editing_router.message.middleware(ErrorMiddleware())
 custom_bot_editing_router.callback_query.middleware(ErrorMiddleware())
 
 admin_bot_menu_router = Router(name="admin_bot_menu")  # only for SUBSCRIBERS
 admin_bot_menu_router.message.filter(ChatTypeFilter(chat_type="private"))
+admin_bot_menu_router.message.outer_middleware(MainLogMiddleware())
 admin_bot_menu_router.message.middleware(CheckSubscriptionMiddleware())
+admin_bot_menu_router.callback_query.outer_middleware(MainLogMiddleware())
 admin_bot_menu_router.callback_query.middleware(CheckSubscriptionMiddleware())
 admin_bot_menu_router.message.middleware(ErrorMiddleware())
 admin_bot_menu_router.callback_query.middleware(ErrorMiddleware())
 
 channel_menu_router = Router(name="channel_menu")  # only for SUBSCRIBERS
 channel_menu_router.message.filter(ChatTypeFilter(chat_type="private"))
+channel_menu_router.message.outer_middleware(MainLogMiddleware())
 channel_menu_router.message.middleware(CheckSubscriptionMiddleware())
+channel_menu_router.callback_query.outer_middleware(MainLogMiddleware())
 channel_menu_router.callback_query.middleware(CheckSubscriptionMiddleware())
 channel_menu_router.message.middleware(ErrorMiddleware())
 channel_menu_router.callback_query.middleware(ErrorMiddleware())
 
 subscribe_router = Router(name="subscribe_router")
 subscribe_router.message.filter(ChatTypeFilter(chat_type="private"))
+subscribe_router.message.outer_middleware(MainLogMiddleware())
 subscribe_router.message.middleware(ErrorMiddleware())
+subscribe_router.callback_query.outer_middleware(MainLogMiddleware())
 subscribe_router.callback_query.middleware(ErrorMiddleware())
 
 commands_router = Router(name="commands")
 commands_router.message.filter(ChatTypeFilter(chat_type="private"))
+commands_router.message.outer_middleware(MainLogMiddleware())
 commands_router.message.middleware(ErrorMiddleware())
+commands_router.callback_query.outer_middleware(MainLogMiddleware())
 commands_router.callback_query.middleware(ErrorMiddleware())
 
---
+++ bot/middlewaries/log_middleware.py
@@ -0,0 +1,53 @@
+import asyncio
+from typing import Callable, Dict, Any, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery, Message, ChatMemberUpdated, TelegramObject, User
+
+from bot.main import bot
+from bot.utils.admin_group import send_event, EventTypes
+
+from logs.config import logger, extra_params
+
+from aiogram.exceptions import TelegramAPIError, TelegramRetryAfter
+
+
+class MainLogMiddleware(BaseMiddleware):
+    async def __call__(
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any]) -> Any:
+        print(1)
+
+        try:
+            user: User = data["event_from_user"]
+            state: FSMContext = data["state"]
+            raw_state: FSMContext = data["raw_state"]
+
+            state_data = await state.get_data()
+            callback_info = f"New event: user_id={user.id}, @{user.username}, {raw_state}, {state_data}"
+
+            if isinstance(event, Message):
+                logger.info(
+                    f"{callback_info} has written {event.text}",
+                    extra=extra_params(user_id=user.id)
+                )
+            elif isinstance(event, CallbackQuery):
+                logger.info(
+                    f"{callback_info} has sent callback_data {event.data}",
+                    extra=extra_params(user_id=user.id)
+                )
+            else:
+                logger.warning(
+                    f"{callback_info} has sent unexpected event {event}",
+                    extra=extra_params(user_id=user.id)
+                )
+        except Exception as e:
+            logger.error(
+                "New event: logger error",
+                exc_info=e
+            )
+
+        return await handler(event, data)
