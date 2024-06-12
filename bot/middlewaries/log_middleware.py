513d6d39cfba40d7e2002257c268495082e34720
zhilovhub <104529663+zhilovhub@users.noreply.github.com>
2024-06-12 13:45:46 +0300

    Logging revolution (added LogMiddleware) (#159)

---
+++ bot/middlewaries/log_middleware.py
@@ -0,0 +1,77 @@
+from logging import Logger
+from typing import Callable, Dict, Any, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery, Message, TelegramObject, User
+
+from logs.config import extra_params
+
+import redis
+
+
+class _MockRedis:
+    """If connect to real Redis is broken"""
+
+    def get(self, *args, **kwargs) -> None:
+        return None
+
+    def set(self, *args, **kwargs) -> None:
+        pass
+
+
+class LogMiddleware(BaseMiddleware):
+    def __init__(self, logger: Logger) -> None:
+        self.logger = logger
+        try:
+            self.redis = redis.Redis()
+            self.redis.info()
+        except redis.ConnectionError as e:
+            self.redis = _MockRedis()
+            self.logger.warning(
+                f"Unable to connect to Redis",
+                exc_info=e
+            )
+
+    async def __call__(
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any]) -> Any:
+
+        try:
+            user: User = data["event_from_user"]
+            state: FSMContext = data["state"]
+            raw_state: FSMContext = data["raw_state"]
+
+            state_data = await state.get_data()
+            callback_info = f"New event: user_id={user.id}, @{user.username}, {raw_state}, {state_data}"
+
+            if isinstance(event, Message):
+                if self.redis.get(str(event.message_id)) is None:
+                    self.logger.info(
+                        f"{callback_info} has written {event.text}",
+                        extra=extra_params(user_id=user.id)
+                    )
+                    # noinspection PyAsyncCall
+                    self.redis.set(name=str(event.message_id), value="", ex=2)
+            elif isinstance(event, CallbackQuery):
+                if self.redis.get(str(event.id)) is None:
+                    self.logger.info(
+                            f"{callback_info} has sent callback_data {event.data}",
+                            extra=extra_params(user_id=user.id)
+                        )
+                    # noinspection PyAsyncCall
+                    self.redis.set(name=str(event.id), value="", ex=2)
+            else:
+                self.logger.warning(
+                    f"{callback_info} has sent unexpected event {event}",
+                    extra=extra_params(user_id=user.id)
+                )
+        except Exception as e:
+            self.logger.error(
+                "New event: logger error",
+                exc_info=e
+            )
+
+        return await handler(event, data)
--- custom_bots/handlers/routers.py
+++ custom_bots/handlers/routers.py
@@ -1,10 +1,20 @@
 from aiogram import Router
 
 from bot.middlewaries.errors_middleware import ErrorMiddleware
+from bot.middlewaries.log_middleware import LogMiddleware
+
+from logs.config import custom_bot_logger
+
+log_middleware = LogMiddleware(logger=custom_bot_logger)
 
 multi_bot_router = Router(name="multibot")
+multi_bot_router.message.outer_middleware(log_middleware)
+multi_bot_router.callback_query.outer_middleware(log_middleware)
 multi_bot_router.message.middleware(ErrorMiddleware())
 multi_bot_router.callback_query.middleware(ErrorMiddleware())
 
 multi_bot_channel_router = Router(name="channels")
+multi_bot_channel_router.message.outer_middleware(log_middleware)
+multi_bot_channel_router.callback_query.outer_middleware(log_middleware)
+multi_bot_channel_router.my_chat_member.outer_middleware(log_middleware)
 multi_bot_channel_router.my_chat_member.middleware(ErrorMiddleware())
