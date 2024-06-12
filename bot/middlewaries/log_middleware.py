e97c72931ae661950ac31c19a834efecea0d9280
unknown <zhilovgithub@gmail.com>
2024-06-12 13:22:13 +0300

    Redis in LogMiddleware

--- bot/middlewaries/log_middleware.py
+++ bot/middlewaries/log_middleware.py
@@ -1,19 +1,18 @@
-import asyncio
 from typing import Callable, Dict, Any, Awaitable
 
 from aiogram import BaseMiddleware
 from aiogram.fsm.context import FSMContext
-from aiogram.types import CallbackQuery, Message, ChatMemberUpdated, TelegramObject, User
-
-from bot.main import bot
-from bot.utils.admin_group import send_event, EventTypes
+from aiogram.types import CallbackQuery, Message, TelegramObject, User
 
 from logs.config import logger, extra_params
 
-from aiogram.exceptions import TelegramAPIError, TelegramRetryAfter
+import redis
 
 
 class MainLogMiddleware(BaseMiddleware):
+    def __init__(self) -> None:
+        self.redis = redis.Redis()
+
     async def __call__(
             self,
             handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
@@ -21,31 +20,34 @@ class MainLogMiddleware(BaseMiddleware):
             data: Dict[str, Any]) -> Any:
 
         try:
-            if not "already_was" in data:
-                data["already_was"] = True
+            user: User = data["event_from_user"]
+            state: FSMContext = data["state"]
+            raw_state: FSMContext = data["raw_state"]
 
-                user: User = data["event_from_user"]
-                state: FSMContext = data["state"]
-                raw_state: FSMContext = data["raw_state"]
+            state_data = await state.get_data()
+            callback_info = f"New event: user_id={user.id}, @{user.username}, {raw_state}, {state_data}"
 
-                state_data = await state.get_data()
-                callback_info = f"New event: user_id={user.id}, @{user.username}, {raw_state}, {state_data}"
-
-                if isinstance(event, Message):
+            if isinstance(event, Message):
+                if self.redis.get(str(event.message_id)) is None:
                     logger.info(
                         f"{callback_info} has written {event.text}",
                         extra=extra_params(user_id=user.id)
                     )
-                elif isinstance(event, CallbackQuery):
+                    # noinspection PyAsyncCall
+                    self.redis.set(name=str(event.message_id), value="", ex=2)
+            elif isinstance(event, CallbackQuery):
+                if self.redis.get(str(event.id)) is None:
                     logger.info(
-                        f"{callback_info} has sent callback_data {event.data}",
-                        extra=extra_params(user_id=user.id)
-                    )
-                else:
-                    logger.warning(
-                        f"{callback_info} has sent unexpected event {event}",
-                        extra=extra_params(user_id=user.id)
-                    )
+                            f"{callback_info} has sent callback_data {event.data}",
+                            extra=extra_params(user_id=user.id)
+                        )
+                    # noinspection PyAsyncCall
+                    self.redis.set(name=str(event.id), value="", ex=2)
+            else:
+                logger.warning(
+                    f"{callback_info} has sent unexpected event {event}",
+                    extra=extra_params(user_id=user.id)
+                )
         except Exception as e:
             logger.error(
                 "New event: logger error",
