6cf8e4af94f3c27d3ee4f4bac8d38f4dcbea2376
Ilya <zhilovgithub@gmail.com>
2024-07-13 11:12:32 +0300

    updated instruciotns

--- bot/utils/send_instructions.py
+++ bot/utils/send_instructions.py
@@ -1,5 +1,5 @@
 from aiogram import Bot
-from aiogram.types import FSInputFile, ReplyKeyboardRemove
+from aiogram.types import FSInputFile, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.exceptions import TelegramBadRequest
 
 from bot import config
@@ -16,20 +16,39 @@ async def send_instructions(
         cache_resources_file_id_store: JsonStore
 ) -> None:
     file_ids = cache_resources_file_id_store.get_data()
+    file_name = "start_instruction.png"
+
+    bot_father_keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="🔐 Получить токен",
+                    url="t.me/BotFather"
+                )
+            ]
+        ]
+    )
+
     try:
-        await bot.send_video(
+        await bot.send_photo(
             chat_id=chat_id,
-            video=file_ids["botfather.mp4"],
+            photo=file_ids[file_name],
             caption=MessageTexts.INSTRUCTION_MESSAGE.value,
-            reply_markup=ReplyBotMenuKeyboard.get_keyboard(bot_id=custom_bot_id)
-            if custom_bot_id else ReplyKeyboardRemove()
+            reply_markup=bot_father_keyboard
         )
+        if custom_bot_id:
+            await bot.send_message(
+                chat_id=chat_id,
+                text="✅ У Вас уже есть бот",
+                reply_markup=ReplyBotMenuKeyboard.get_keyboard(bot_id=custom_bot_id)
+            )
     except (TelegramBadRequest, KeyError) as e:
         logger.info(f"error while sending instructions.... cache is empty, sending raw files {e}")
-        video_message = await bot.send_video(
+        instruction_message = await bot.send_photo(
             chat_id=chat_id,
-            video=FSInputFile(config.RESOURCES_PATH.format("botfather.mp4")),
-            caption=MessageTexts.INSTRUCTION_MESSAGE.value
+            photo=FSInputFile(config.RESOURCES_PATH.format(file_name)),
+            caption=MessageTexts.INSTRUCTION_MESSAGE.value,
+            reply_markup=bot_father_keyboard
         )
-        file_ids[f"botfather.mp4"] = video_message.video.file_id
+        file_ids[file_name] = instruction_message.photo[-1].file_id
         cache_resources_file_id_store.update_data(file_ids)
