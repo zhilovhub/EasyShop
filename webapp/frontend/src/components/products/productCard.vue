c03d154f10582ef68fd59b0fd1dde1c9c9021aae
Vovan533 <vovan533v@gmail.com>
2024-07-16 21:21:03 +0300

    test

--- webapp/frontend/src/components/products/productCard.vue
+++ webapp/frontend/src/components/products/productCard.vue
@@ -165,7 +165,7 @@ export default {
       if (this.productObject.chosenOption === undefined) {
         this.productObject.chosenOption = []
       }
-      console.log(allOptions)
+      console.log(this.productObject.extra_options)
       this.productObject.chosenOption.push({name: "test", selected_variant: target.innerText});
       if (key && key > 0) {
         this.productObject.price = this.productObject.extra_options[0].variants_prices[key];
