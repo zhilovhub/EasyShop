80d7778440eabadefd83c98eac175e9702d2a47f
zhilovhub <104529663+zhilovhub@users.noreply.github.com>
2024-07-19 17:19:05 +0300

    fixed border bug in productcard (#188)
    Co-authored-by: karfogent <karfogent3@gmail.com>

--- webapp/frontend/src/components/products/productCard.vue
+++ webapp/frontend/src/components/products/productCard.vue
@@ -54,7 +54,7 @@
         <div class="span-block">
           <h1>{{option.name}}</h1>
           <svg style="cursor: pointer" width="19" height="9" viewBox="0 0 19 9" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path d="M19.0032 0.796349C19.0038 0.900537 18.9839 1.00382 18.9445 1.10028C18.9051 1.19673 18.847 1.28446 18.7737 1.35843L12.3057 7.82635C11.938 8.19497 11.5012 8.48744 11.0203 8.68699C10.5394 8.88654 10.0239 8.98926 9.50324 8.98926C8.98258 8.98926 8.46704 8.88654 7.98614 8.68699C7.50524 8.48744 7.06843 8.19497 6.70074 7.82635L0.232823 1.35843C0.159009 1.28462 0.100456 1.19699 0.0605087 1.10055C0.0205609 1.0041 9.72324e-08 0.900737 9.76995e-08 0.796349C9.81665e-08 0.691959 0.0205609 0.588593 0.0605087 0.492151C0.100456 0.395709 0.159009 0.30808 0.232823 0.234265C0.306636 0.160452 0.394266 0.101898 0.490709 0.0619507C0.587151 0.0220032 0.690517 0.00144292 0.794906 0.00144292C0.899294 0.00144292 1.00266 0.0220032 1.0991 0.0619507C1.19555 0.101898 1.28318 0.160452 1.35699 0.234265L7.82491 6.70218C8.27022 7.14694 8.87386 7.39676 9.50324 7.39676C10.1326 7.39676 10.7363 7.14694 11.1816 6.70218L17.6495 0.234266C17.7231 0.160064 17.8106 0.101168 17.9071 0.0609753C18.0036 0.0207836 18.1071 9.17687e-05 18.2116 9.17699e-05C18.3161 9.17712e-05 18.4196 0.0207836 18.516 0.0609753C18.6125 0.101168 18.7001 0.160064 18.7737 0.234266C18.847 0.308239 18.9051 0.395968 18.9445 0.492422C18.9839 0.588877 19.0038 0.69216 19.0032 0.796349Z" fill="currentColor"/>
+            <path d="M19Z" fill="currentColor"/>
           </svg>
         </div>
         <swiper
@@ -72,12 +72,12 @@
             @click.stop
             style="background-color: var(--app-card-background-color); height: 120px"
           >
-            <div style="display: flex; flex-direction: column">
+            <div class="priced_block">
               <div style="background-color: var(--app-background-color); height: 64px; width: 75px; border-radius: 15px; display: flex; justify-content: center; align-items: center">
                 <span>{{ value }}</span>
               </div>
               <div style="background-color: var(--app-background-color); height: 42px; width: 75px; border-radius: 15px; margin-top: 5px; display: flex; justify-content: center; align-items: center">
-                <span>{{ option.variants_prices[key]}}₽</span>
+                <span>{{ option.variants_prices[key] }}₽</span>
               </div>
             </div>
           </swiper-slide>
@@ -157,25 +157,37 @@ export default {
       return parts.join(' ') + '₽';
     },
     chooseOption(target, key) {
-      const allOptions = document.querySelectorAll('.option-block');
-      allOptions.forEach(option => {
-        option.classList.remove('chosen');
-      });
-      target.classList.add('chosen');
-      if (this.productObject.chosenOption === undefined) {
-        this.productObject.chosenOption = []
+      // Ищем родительский элемент с классом 'option-block'
+      while (target && !target.classList.contains('option-block')) {
+        target = target.parentElement;
       }
-      console.log(this.productObject.extra_options)
-      let name = null
+
+      if (!target) return;
+
+      // Удаляем класс 'chosen' у всех опций
+      const allOptions = document.querySelectorAll('.option-block');
+      allOptions.forEach(option => option.classList.remove('chosen'));
+
+      target.classList.add('chosen');
+
+      if (!this.productObject.chosenOption) {
+        this.productObject.chosenOption = [];
+      }
+
+      const selectedText = target.innerText.split('\n')[0].trim();
+      let optionName = null;
       for (let option of this.productObject.extra_options) {
-        for (let variant of option.variants) {
-          if ((name === null) && (variant === target.innerText)){
-            name = option.name
-          }
+        if (option.variants.includes(selectedText)) {
+          optionName = option.name;
+          break;
         }
       }
-      this.productObject.chosenOption.push({name: name, selected_variant: target.innerText});
-      if (key && key > 0) {
+
+      if (optionName) {
+        this.productObject.chosenOption.push({name: optionName, selected_variant: target.innerText});
+      }
+
+      if (this.productObject.extra_options[0].variants_prices[key]) {
         this.productObject.price = this.productObject.extra_options[0].variants_prices[key];
       }
     },
@@ -392,5 +404,10 @@ button {
   &:hover{
     background-color: #82ccec;
   }
+}
+
+.priced_block {
+  display: flex;
+  flex-direction: column;
 }
 </style>
