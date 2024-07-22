4eccf2f098999580a59aefe73020081a5015e5c2
karfogent <karfogent3@gmail.com>
2024-07-22 12:09:52 +0300

    added bg-color mechanic

--- webapp/frontend/src/main.js
+++ webapp/frontend/src/main.js
@@ -23,24 +23,6 @@ import {Store} from "./store/store.js"
 
 Store.state.bot_id = new URL(window.location.href).searchParams.get('bot_id');
 
-async function getWebAppOptions() {
-    try {
-        console.log(Store.state.bot_id);
-        const response = await fetch(`${Store.state.api_url}/api/settings/get_web_app_options/${Store.state.bot_id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'authorization-data': 'DEBUG',
-            },
-        });
-        console.log(response);
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-    }
-}
-getWebAppOptions();
-
 const app = createApp(App)
 // eslint-disable-next-line vue/multi-word-component-names
 
