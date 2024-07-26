4fd076bc0fb9f316e20075fca516f6dc1ea179f2
unknown <zhilovgithub@gmail.com>
2024-07-26 12:29:26 +0300

    updated /start 1.0

--- bot/middlewaries/subscription_middleware.py
+++ bot/middlewaries/subscription_middleware.py
@@ -1,5 +1,4 @@
 from typing import Callable, Dict, Any, Awaitable
-from datetime import datetime
 
 from aiogram import BaseMiddleware
 from aiogram.types import CallbackQuery, Message
@@ -8,15 +7,14 @@ from bot.main import subscription
 from bot.utils.check_subscription import check_subscription
 
 from common_utils.env_config import ADMINS
-from common_utils.broadcasting.broadcasting import EventTypes, send_event
-
-from database.config import user_db
-from database.models.user_model import UserSchema, UserStatusValues, UserNotFoundError
-
-from logs.config import logger
 
 
 class CheckSubscriptionMiddleware(BaseMiddleware):
+    """Даем доступ, если пользователь:
+    1. Админ
+    2. Имеет статус TRIAL, SUBSCRIBED
+    """
+
     async def __call__(
             self,
             handler: Callable[[CallbackQuery | Message, Dict[str, Any]], Awaitable[Any]],
@@ -25,26 +23,10 @@ class CheckSubscriptionMiddleware(BaseMiddleware):
     ) -> Any:
         user_id = event.from_user.id
         message, is_message = (event, True) if isinstance(event, Message) else (event.message, False)
-        try:
-            await user_db.get_user(user_id)
-        except UserNotFoundError:
-            logger.info(f"user {user_id} not found in db, creating new instance...")
-            await send_event(event.from_user, EventTypes.NEW_USER)
-            await user_db.add_user(UserSchema(
-                user_id=user_id, username=event.from_user.username, registered_at=datetime.utcnow(),
-                status=UserStatusValues.TRIAL, locale="default", subscribed_until=None)
-            )
-            logger.error(
-                f"user_id={user_id}: DANGER! This place in middleware should not be called"
-            )
 
-        if user_id not in ADMINS and \
-                not (await subscription.is_user_subscribed(user_id)) and \
-                message.text not in ("/start", "/check_subscription"):
-            if is_message:
-                await message.answer("Для того, чтобы пользоваться ботом, тебе нужна подписка")
-            else:
-                await event.answer("Для того, чтобы пользоваться ботом, тебе нужна подписка", show_alert=True)
+        if user_id not in ADMINS and not (await subscription.is_user_subscribed(user_id)):
+            # Пользователь не прошел ни по одному из заявленных критериев, уведомляем его об этом
             return await check_subscription(message)
 
+        # Полноценный доступ открыт - исполняем хендлер
         return await handler(event, data)
