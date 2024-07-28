a93c05d198a77e9bdcfcc796f47f5fb15e5bc04f
unknown <zhilovgithub@gmail.com>
2024-07-28 11:31:29 +0300

    changed debug scripts and version WEBHOOK_SERVER_HOST_TO_REDIRECT + WEBHOOK_LABEL

--- server/debug_scripts/disable.sh
+++ server/debug_scripts/disable.sh
@@ -1,17 +1,25 @@
+if [ $# -ne 1 ]
+then
+  "There must be only one argument - label (debug, arsen, vova, ilya, front)"
+  exit 1
+fi
+
+label=$1
+
 echo "[DEBUG] Stopping Project..."
 echo "[DEBUG] Stopping API service... [Step 1/3]"
-systemctl --user disable debug_api.service
-systemctl --user stop debug_api.service
-systemctl --user status debug_api.service | grep Loaded
-systemctl --user status debug_api.service | grep Active
+systemctl --user disable dev_api@${label}.service
+systemctl --user stop dev_api@${label}.service
+systemctl --user status dev_api@${label}.service | grep Loaded
+systemctl --user status dev_api@${label}.service | grep Active
 echo "[DEBUG] Stopping Telegram bot service... [Step 2/3]"
-systemctl --user disable debug_bot.service
-systemctl --user stop debug_bot.service
-systemctl --user status debug_bot.service | grep Loaded
-systemctl --user status debug_bot.service | grep Active
+systemctl --user disable dev_bot@.service
+systemctl --user stop dev_bot@${label}.service
+systemctl --user status dev_bot@${label}.service | grep Loaded
+systemctl --user status dev_bot@${label}.service | grep Active
 echo "[DEBUG] Stopping Multi bot service... [Step 3/3]"
-systemctl --user disable debug_multibot.service
-systemctl --user stop debug_multibot.service
-systemctl --user status debug_multibot.service | grep Loaded
-systemctl --user status debug_multibot.service | grep Active
+systemctl --user disable dev_multibot@.service
+systemctl --user stop dev_multibot@${label}.service
+systemctl --user status dev_multibot@${label}.service | grep Loaded
+systemctl --user status dev_multibot@${label}.service | grep Active
 
--- server/debug_scripts/status.sh
+++ server/debug_scripts/status.sh
@@ -1,11 +1,22 @@
+if [ $# -ne 1 ]
+then
+  "There must be only one argument - label (debug, arsen, vova, ilya, front)"
+  exit 1
+fi
+
+label=$1
+
 echo "[DEBUG] Getting services status..."
+
 echo "[DEBUG] API service [1/3]"
-systemctl --user status debug_api.service | grep Loaded
-systemctl --user status debug_api.service | grep Active
+systemctl --user status dev_api@${label}.service | grep Loaded
+systemctl --user status dev_api@${label}.service | grep Active
+
 echo "[DEBUG] Telegram bot service [2/3]"
-systemctl --user status debug_bot.service | grep Loaded
-systemctl --user status debug_bot.service | grep Active
+systemctl --user status dev_bot@${label}.service | grep Loaded
+systemctl --user status dev_bot@${label}.service | grep Active
+
 echo "MultiBot service [3/3]"
-systemctl --user status debug_multibot.service | grep Loaded
-systemctl --user status debug_multibot.service | grep Active
+systemctl --user status dev_multibot@${label}.service | grep Loaded
+systemctl --user status dev_multibot@${label}.service | grep Active
 
