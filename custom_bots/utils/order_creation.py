1208cbc4333541f46bae78514385f96764783dd8
Vovan533 <vovan533v@gmail.com>
2024-07-31 15:50:19 +0300

    fix order creation

--- custom_bots/utils/order_creation.py
+++ custom_bots/utils/order_creation.py
@@ -1,5 +1,7 @@
+from common_utils.bot_utils import create_bot_options
+from database.models.option_model import OptionNotFoundError
 from database.models.order_model import OrderSchema
-from database.config import order_db, product_db, bot_db
+from database.config import order_db, product_db, bot_db, option_db
 
 from aiogram import Bot
 from aiogram.enums import ParseMode
@@ -7,9 +9,9 @@ from aiogram.types import Chat
 
 from custom_bots.multibot import main_bot, PREV_ORDER_MSGS
 
-from custom_bots.utils.custom_bot_options import get_option
 from custom_bots.utils.custom_message_texts import CustomMessageTexts
 
+from common_utils.message_texts import MessageTexts as CommonMessageTexts
 from common_utils.keyboards.order_manage_keyboards import InlineOrderStatusesKeyboard, InlineOrderCustomBotKeyboard
 
 from logs.config import extra_params, custom_bot_logger
@@ -19,27 +21,41 @@ async def order_creation_process(order: OrderSchema, order_user_data: Chat):
     bot_id = order.bot_id
     user_id = order_user_data.id
 
-    await order_db.add_order(order)
-
-    products = [(await product_db.get_product(product_id), product_item.amount, product_item.used_extra_options)
-                for product_id, product_item in order.items.items()]
-    username = "@" + order_user_data.username if order_user_data.username else order_user_data.full_name
-
     custom_bot = await bot_db.get_bot(bot_id)
     custom_bot_tg = Bot(custom_bot.token)
 
+    await order_db.add_order(order)
+
+    products = []
+    for product_id, order_item in order.items.items():
+        product = await product_db.get_product(product_id)
+        products.append((product, order_item.amount, order_item.used_extra_options))
+    username = "@" + order_user_data.username if order_user_data.username else order_user_data.full_name
+
+    # TODO Handle exceptions
+    custom_bot = await bot_db.get_bot_by_token(custom_bot.token)
+
     admin_id = custom_bot.created_by
 
+    text = await CommonMessageTexts.generate_order_notification_text(
+        order,
+        products,
+        username,
+        True
+    )
     main_msg = await main_bot.send_message(
-        admin_id, order.convert_to_notification_text(
-            products,
-            username,
-            True
-        ))
-
+        admin_id, **text
+    )
     products_to_refill = []
     products_not_enough = []
-    if await get_option("auto_reduce", custom_bot.token) is True:
+    try:
+        options = await option_db.get_option(custom_bot.options_id)
+    except OptionNotFoundError:
+        new_options_id = await create_bot_options()
+        custom_bot.options_id = new_options_id
+        await bot_db.update_bot(custom_bot)
+        options = await option_db.get_option(new_options_id)
+    if options.auto_reduce is True:
         for ind, product_item in enumerate(products, start=1):
             product_schema, amount, extra_options = product_item
             if product_schema.count <= amount:
@@ -50,12 +66,14 @@ async def order_creation_process(order: OrderSchema, order_user_data: Chat):
     msg_id_data = PREV_ORDER_MSGS.get_data()
     msg_id_data[order.id] = (main_msg.chat.id, main_msg.message_id)
     PREV_ORDER_MSGS.update_data(msg_id_data)
+    text = await CommonMessageTexts.generate_order_notification_text(
+        order,
+        products,
+        username,
+        False
+    )
     msg = await custom_bot_tg.send_message(
-        user_id, order.convert_to_notification_text(
-            products,
-            username,
-            False
-        ), reply_markup=InlineOrderCustomBotKeyboard.get_keyboard(order.id), parse_mode=ParseMode.HTML
+        chat_id=user_id, reply_markup=InlineOrderCustomBotKeyboard.get_keyboard(order.id), **text
     )
 
     if len(products_to_refill) != 0:
@@ -71,7 +89,7 @@ async def order_creation_process(order: OrderSchema, order_user_data: Chat):
             **CustomMessageTexts.generate_not_enough_in_stock(products_not_enough, order.id)
         )
 
-    post_order_text = await get_option("post_order_msg", custom_bot.token)
+    post_order_text = options.post_order_msg
     if post_order_text:
         await custom_bot_tg.send_message(
             chat_id=user_id,
