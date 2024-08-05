9147ee599c0d63778bdb373e52d9700b2a4242bf
Ilya <zhilovgithub@gmail.com>
2024-08-05 15:41:01 +0300

    fixed many connections in bot_update_options.py

--- administration/bot_update_options.py
+++ administration/bot_update_options.py
@@ -29,30 +29,31 @@ db_order_option: OrderOptionDao = database.get_order_option_dao()
 async def main() -> None:
     all_bots = await db_bot.get_bots()
     for bot in all_bots:
-        bg_color = bot.settings.get("bg_color", None)
-        start_msg = bot.settings.get("start_msg", MessageTexts.DEFAULT_START_MESSAGE.value)
-        default_msg = bot.settings.get(
-            "default_msg", f"Приветствую, этот бот создан с помощью @{(await Bot(token=bot.token).get_me()).username}"
-        )
-        auto_reduce = bot.settings.get("auto_reduce", True)
-        web_app_button = bot.settings.get("web_app_button", MessageTexts.OPEN_WEB_APP_BUTTON_TEXT.value)
-        post_order_msg = bot.settings.get("post_order_msg", None)
+        if not bot.options_id:
+            bg_color = bot.settings.get("bg_color", None)
+            start_msg = bot.settings.get("start_msg", MessageTexts.DEFAULT_START_MESSAGE.value)
+            default_msg = bot.settings.get(
+                "default_msg", f"Приветствую, этот бот создан с помощью @{(await Bot(token=bot.token).get_me()).username}"
+            )
+            auto_reduce = bot.settings.get("auto_reduce", True)
+            web_app_button = bot.settings.get("web_app_button", MessageTexts.OPEN_WEB_APP_BUTTON_TEXT.value)
+            post_order_msg = bot.settings.get("post_order_msg", None)
 
-        options = OptionSchemaWithoutId(
-            start_msg=start_msg,
-            default_msg=default_msg,
-            post_order_msg=post_order_msg,
-            auto_reduce=auto_reduce,
-            bg_color=bg_color,
-            web_app_button=web_app_button
-        )
+            options = OptionSchemaWithoutId(
+                start_msg=start_msg,
+                default_msg=default_msg,
+                post_order_msg=post_order_msg,
+                auto_reduce=auto_reduce,
+                bg_color=bg_color,
+                web_app_button=web_app_button
+            )
 
-        option_id = await db_options.add_option(options)
-        bot.options_id = option_id
-        await db_bot.update_bot(bot)
+            option_id = await db_options.add_option(options)
+            bot.options_id = option_id
+            await db_bot.update_bot(bot)
 
-        await create_order_options(order_option_db=db_order_option, bot_id=bot.bot_id)
-        print(bot, option_id)
+            await create_order_options(order_option_db=db_order_option, bot_id=bot.bot_id)
+            print(bot, option_id)
 
 
 if __name__ == '__main__':
