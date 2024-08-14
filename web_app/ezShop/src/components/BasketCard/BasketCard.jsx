79b9e1503542c7c97b3ea9f99a5a48bebbe6a8f1
Kirill <lvovkir@yandex.ru>
2024-08-14 13:36:06 +0300

    fix port

--- web_app/ezShop/src/components/BasketCard/BasketCard.jsx
+++ web_app/ezShop/src/components/BasketCard/BasketCard.jsx
@@ -16,7 +16,7 @@ function BasketCard(props) {
     const [imgUrl, setImgUrl] = useState('')
 
 
-    const url = `https://ezbots.ru:2024/api/files/get_file/${props.product.pictures[0]}`;
+    const url = `https://ezbots.ru:1537/api/files/get_file/${props.product.pictures[0]}`;
         fetch(url, {
             method: 'GET',
             headers: {
