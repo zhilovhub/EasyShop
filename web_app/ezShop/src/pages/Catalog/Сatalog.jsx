86143a5e6a153f31f98609edd170284e6a0af556
Kirill <lvovkir@yandex.ru>
2024-08-14 19:47:29 +0300

    fix add botId 2

--- web_app/ezShop/src/pages/Catalog/Сatalog.jsx
+++ web_app/ezShop/src/pages/Catalog/Сatalog.jsx
@@ -44,7 +44,7 @@ function Catalog() {
 
         if (productList.length == 0){
 
-        const url = `https://ezbots.ru:1537/api/products/get_all_products?bot_id=${botId}`;
+        const url = `https://ezbots.ru:1537/api/products/get_all_products?bot_id=${newBotId}`;
         const body = JSON.stringify([]);
         fetch(url, {
             method: 'POST',
