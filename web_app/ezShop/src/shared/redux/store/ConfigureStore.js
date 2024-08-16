3ac4f2e606ea8278f9868fa85b17a712ff557090
Kirill <lvovkir@yandex.ru>
2024-08-16 19:53:01 +0300

    edit option in Redux

--- web_app/ezShop/src/shared/redux/store/ConfigureStore.js
+++ web_app/ezShop/src/shared/redux/store/ConfigureStore.js
@@ -4,13 +4,15 @@ import productListReducer from '../reducer/ProductListReducer';
 import filterReducer from '../reducer/FilterReducer';
 import validateReducer from '../reducer/ValidateReducer';
 import botIdReducer from '../reducer/BotIdReducer';
+import orderDataReducer from '../reducer/OrderDataReducer';
 
 const rootReducer = combineReducers({
   language: languageReducer,
   productList: productListReducer,
   filter: filterReducer,
   validate: validateReducer,
-  botId: botIdReducer
+  botId: botIdReducer,
+  orderData: orderDataReducer
 });
 
 const store = createStore(rootReducer);
