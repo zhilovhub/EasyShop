38e3b21d587a018eb05e2f9dbd710fd1cfa2ab47
Ilya <zhilovgithub@gmail.com>
2024-08-28 11:53:00 +0300

    administration/build_referral_tree.py init

---
+++ administration/build_referral_tree.py
@@ -0,0 +1,54 @@
+import asyncio
+
+from aiogram import Bot
+
+from common_utils.config import main_telegram_bot_settings
+
+from database.models.models import Database
+from database.models.referral_invite_model import ReferralInviteDao
+
+from logs.config import db_logger
+
+
+database: Database = Database(sqlalchemy_url="postgresql+asyncpg://database_module:Fewe34_!dfm44GG@92.118.114.106:5432/easybots_db", logger=db_logger, unique_id="util")
+referral_invite_db: ReferralInviteDao = database.get_referral_invite_dao()
+
+
+async def main() -> None:
+    bot = Bot("6851803815:AAH8SF9kkkmlt9IJcO2NmhRk9NsG5HVyy04")
+    cache = {}
+
+    all_invites = await referral_invite_db.get_all_invites()
+    chains = []
+    for ind, invite in enumerate(all_invites, start=1):
+        chain = [invite.user_id]
+        if invite.came_from:
+            temp_invite = invite.model_copy()
+            while temp_invite.came_from:
+                came_from = temp_invite.came_from
+                chain.append(came_from)
+                temp_invite = await referral_invite_db.get_invite_by_user_id(came_from)
+            chains.append(" -> ".join([await _convert_id_to_username(bot, cache, x) for x in  chain[::-1]]))
+
+        print(f"{ind}/{len(all_invites)}: invite has been handled")
+
+
+    print(*chains, sep="\n")
+
+
+async def _convert_id_to_username(bot: Bot, cache: dict, user_id: int) -> str:
+    if user_id in cache:
+        return cache[user_id]
+
+    chat = await bot.get_chat(chat_id=user_id)
+    if chat.username:
+        username = "@" + chat.username
+    else:
+        username = str(user_id)
+
+    cache[user_id] = username
+    return username
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
